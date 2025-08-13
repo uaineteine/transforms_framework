f047778f002ec7c41f497c9ff28fa9495cc690eb
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-08-14 09:34:47 +1000

    simplification of variable lists and types

--- transforms_class.py
+++ transforms_class.py
@@ -8,11 +8,6 @@ class Transform(PipelineEvent):
         super().__init__(event_type="transform", message=name, description=description, log_location="events_log/job_1/transforms.json")
         self.name = name  # Set name manually
         self.transform_type = transform_type
-
-        #initalise
-        self.created_variables = None
-        self.renamed_variables = None
-        self.deleted_variables = None
     
     def transforms(self, df:DataFrame, df2:DataFrame=None):
         raise NotImplementedError("Subclasses should implement this method.")
@@ -36,18 +31,27 @@ class Transform(PipelineEvent):
         return result_df
 
 class TableTransform(Transform):
-    def __init__(self, name: str, description: str, acts_on_variable: str):
+    def __init__(self, name: str, description: str, acts_on_variables: list[str]):
         super().__init__(name, description, "TableTransform")
-        self.target_table = "Uncalled"
-        self.target_variable = acts_on_variable
+        self.target_tables = [] #nadah to begin with
+        self.target_variables = acts_on_variables
+        
+        #initalise
+        self.created_variables = None
+        self.renamed_variables = None
+        self.deleted_variables = None
 
-class DropVariable(TableTransform):
+class SimpleTransform(TableTransform):
+    def __init__(self, name: str, description: str, acts_on_variable: str):
+        super().__init__(name, description, [acts_on_variable])
+
+class DropVariable(SimpleTransform):
     def __init__(self, variable_to_drop: str):
         super().__init__("DropVariable", "Removes this variable from a dataframe", variable_to_drop)
 
     def transforms(self, tbl: Metaframe, tbl2: Metaframe = None):
         if self.target_variable not in tbl.df.columns:
-            raise ValueError(f"Variable '{self.target_variable}' not found in DataFrame columns: {tbl.df.columns}")
+            raise ValueError(f"Variable '{self.target_variables}' not found in DataFrame columns: {tbl.df.columns}")
         
         self.deleted_variables = [self.target_variable]
         self.target_table = tbl.table_name
