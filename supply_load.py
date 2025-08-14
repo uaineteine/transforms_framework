d2e43663e31988fc09b641e13e4c3d29e89a4fb4
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-08-15 01:36:57 +1000

    trial loading

--- supply_load.py
+++ supply_load.py
@@ -1,9 +1,10 @@
 import json
+from metaframe import MetaFrame
 from pipeline_table import PipelineTable
 
 class SupplyLoad:
     """Class to handle supply load information from a JSON file."""
-    def __init__(self, json_loc:str):
+    def __init__(self, json_loc:str, spark=None):
         if json_loc = "":
             raise ValueError("Supply JSON path cannot be empty")
         
@@ -11,17 +12,24 @@ class SupplyLoad:
 
         #return whole list of PipelineTables for working with the supply load
         self.tables = [] # List to hold supply load tables
+        self.load_supplies(spark)
 
-    def load_supplies(self):
+    def load_supplies(self, spark=None):
         """Load supply data from the JSON file."""
         try:
             with open(self.supply_load_src, 'r') as file:
                 data = json.load(file)
-                self.supply = data.get("supply", [])
-                for item in self.supply:
-                    table = PipelineTable(item["name"], item["format"], item["path"])
+                supply = data.get("supply", [])
+                for item in supply:
+                    mf = MetaFrame.load(item["path"], item["format"], "pyspark", spark)
+                    table = PipelineTable(mf)
                     self.tables.append(table)
+
         except FileNotFoundError:
             raise FileNotFoundError(f"Supply JSON file not found at {self.supply_load_src}")
+        
         except json.JSONDecodeError:
-            raise ValueError("Invalid JSON format in supply load file"
+            raise ValueError("Invalid JSON format in supply load file")
+        
+        except Exception as e:
+            raise RuntimeError(f"An error occurred while loading supply data: {str(e)}")
