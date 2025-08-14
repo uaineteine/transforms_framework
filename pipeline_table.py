35fc755bdf76a8c6eda70ade3aa6221bd3e82ecd
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-08-14 23:04:58 +1000

    add more info to load

--- pipeline_table.py
+++ pipeline_table.py
@@ -30,7 +30,7 @@ class PipelineTable(MetaFrame):
         Append the events list
         """
         self.events.append(event)
-        
+
     @staticmethod
     def load(path:str, format: str = "parquet", table_name: str = "", frame_type: str = FrameTypeVerifier.pyspark, spark=None):
         """
@@ -45,7 +45,7 @@ class PipelineTable(MetaFrame):
         """
 
         mf = MetaFrame.load(path, format, table_name, frame_type, spark)
-        event = PipelineEvent(event_type="load", message=f"Loaded table from {path} as {format} ({frame_type})", description=f"Loaded {table_name} from {path}")
+        event = PipelineEvent(event_type="load", message=f"Loaded table from {path} as {format} ({frame_type})", description=f"Loaded {table_name} from {path} with version {self.pipeline_table_version}")
 
         ptable = PipelineTable(mf)
         ptable.add_event(event)
