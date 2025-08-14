6a4b15a2ab0f8f40d4bb2c3f320a872dc0cc58bc
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-08-14 23:05:46 +1000

    fix

--- pipeline_table.py
+++ pipeline_table.py
@@ -45,9 +45,9 @@ class PipelineTable(MetaFrame):
         """
 
         mf = MetaFrame.load(path, format, table_name, frame_type, spark)
-        event = PipelineEvent(event_type="load", message=f"Loaded table from {path} as {format} ({frame_type})", description=f"Loaded {table_name} from {path} with version {self.pipeline_table_version}")
 
         ptable = PipelineTable(mf)
+        event = PipelineEvent(event_type="load", message=f"Loaded table from {path} as {format} ({frame_type})", description=f"Loaded {table_name} from {path} with version {ptable.pipeline_table_version}")
         ptable.add_event(event)
         return ptable
 
