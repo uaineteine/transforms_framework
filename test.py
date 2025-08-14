af5cbe558b7f679626f3c467617c151bce595891
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-08-14 22:47:19 +1000

    updated test

--- test.py
+++ test.py
@@ -1,21 +1,21 @@
 if __name__ == "__main__":
     from pyspark.sql import SparkSession
     from transforms_class import DropVariable
-    from metaframe import Metaframe
+    from pipeline_table import PipelineTable
 
     # Create Spark session
     print("Creating Spark session")
     spark = SparkSession.builder.master("local").appName("TransformTest").getOrCreate()
 
-    # Wrap DataFrame in Metaframe
-    mf = Metaframe.load(spark=spark, path="test.csv", format="csv", table_name="test_table", frame_type="pyspark")
-    print("Original columns:", tbl.df.columns)
+    # load pipeline table
+    mf = PipelineTable.load(spark=spark, path="test.csv", format="csv", table_name="test_table", frame_type="pyspark")
+    print("Original columns:", mf.df.columns)
 
     # Instantiate DropVariable transform
-    mf = DropVariable("age")(tbl)
+    mf = DropVariable("age")(mf)
 
     # Show result
-    print("Transformed columns:", tbl.df.columns)
+    print("Transformed columns:", mf.df.columns)
     mf.df.show()
 
 
