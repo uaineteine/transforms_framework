cc821aec77433f86baaf88b3934ff0d2fad70316
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-08-14 22:17:32 +1000

    metaframe type created

--- metaframe.py
+++ metaframe.py
@@ -60,6 +60,7 @@ class Metaframe:
         return self.table_name
 
     def __str__(self):
+        #JSON like string representation
         return f"Metaframe(name={self.table_name}, type={self.frame_type})"
 
     def get_pandas_frame(self):
@@ -96,16 +97,16 @@ class Metaframe:
             raise ValueError("Unsupported frame_type")
 
     @staticmethod
-    def load(spark=None, path: str = "", format: str = "parquet", table_name: str = "", frame_type: str = FrameTypeVerifier.pyspark):
+    def load(path:str, format: str = "parquet", table_name: str = "", frame_type: str = FrameTypeVerifier.pyspark, spark=None):
         """
-        Load a DataFrame from the given path and return a MetaplusTable.
+        Load a DataFrame from the given path and return a Metaframe.
 
-        :param spark: SparkSession object (required for PySpark).
         :param path: Path to the data file.
         :param format: File format (default: 'parquet').
         :param table_name: Optional table name.
         :param frame_type: Type of DataFrame ('pyspark', 'pandas', 'polars').
-        :return: MetaplusTable instance.
+        :param spark: SparkSession object (required for PySpark).
+        :return: Metaframe instance.
         """
         if frame_type == "pyspark":
             if spark is None:
@@ -138,22 +139,4 @@ class Metaframe:
             raise ValueError("Unsupported frame_type")
 
         tbl = Metaframe(df, src_path=path, table_name=table_name, frame_type=frame_type)
-        event = PipelineEvent(event_type="load", message=f"Loaded table from {path} as {format} ({frame_type})", description=f"Loaded {table_name} from {path}")
-        tbl.events.append(event)
-        return tbl
-
-    def save_events(self):
-        """
-        Save the events to a JSON file in the events_log directory.
-
-        :return: None
-        """
-        if not os.path.exists("events_log"):
-            os.makedirs("events_log")
-        
-        log_path = f"events_log/job_1/{self.table_name}_events.json"
-        for event in self.events:
-            event.log_location = log_path
-            event.log()
-        
-        print(f"Events saved to {log_path}")
+        return table
