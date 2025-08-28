9627479399d03fdb355f2ffaa01951df43003a54
Daniel <39141263+uaineteine@users.noreply.github.com>
2025-08-29 08:50:33 +1000

    Starting stub for macros

---
+++ transformslib/transforms/macrolib.py
@@ -0,0 +1,18 @@
+#to be renamed
+from transformslib.tables.collections.collection import TableCollection 
+from transformslib.transforms.atomiclib import *
+
+class Macro:
+  def __init__(self,
+               input_tables:TableCollection,
+               output_tables:list[str],
+               input_variables:list[str],
+               output_varibales:list[str]
+              ):
+
+def TopBottomCode(input_tables:TableCollection,
+               output_tables:list[str],
+               input_variables:list[str],
+               output_variables:list[str]
+              ):
+  
