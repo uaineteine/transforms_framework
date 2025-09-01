0d45c29bd87ac7dc0a5e21249dd36e3b22edecce
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-09-01 21:28:50 +1000

    transform log reader function

--- transformslib/mapping/dagnew.py
+++ transformslib/mapping/dagnew.py
@@ -4,10 +4,17 @@ import networkx as nx
 import json
 import os
 
-# Load logs
-log_file = os.path.join(os.path.dirname(__file__), "transform_log.json")
-with open(log_file, "r", encoding="utf-8") as f:
-    logs = json.load(f)
+main_dir = "events_log"
+
+def load_transform_log(job_id, run_id):
+    log_file = os.path.join(f"job_{job_id}", "transforms.json")
+    if not os.path.exists(log_file):
+        raise FileNotFoundError(f"Log file not found: {log_file}")
+    else:
+        with open(log_file, "r", encoding="utf-8") as f:
+            return json.load(f)
+
+logs = load_transform_log(job_id=1, run_id=1)
 
 # Build DAG
 input_dfs = [log["params"]["path"] for log in logs if log["function"].startswith("read_file")]
