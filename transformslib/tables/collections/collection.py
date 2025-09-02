8e1b9fea64143458d916c32ee9b35bc84b80cade
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-09-02 22:46:36 +1000

    path simplification

--- transformslib/tables/collections/collection.py
+++ transformslib/tables/collections/collection.py
@@ -404,7 +404,7 @@ class TableCollection:
         os.makedirs(output_dir, exist_ok=True)
 
         for table in self.tables:
-            output_path = output_dir + "/" + table.table_name + ".parquet"
+            output_path = os.path.join(output_dir, table.table_name + ".parquet")
             table.write(path=output_path, spark=spark)
         
         self.save_events()
