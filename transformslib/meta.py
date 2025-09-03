739ea74efdd3edef3df69cbf23913cb2bf6a5791
Daniel Stamer-Squair <daniel.robert.s-s@hotmail.com>
2025-09-03 17:44:23 +1000

    new meta version check

--- transformslib/meta.py
+++ transformslib/meta.py
@@ -11,3 +11,7 @@ module_version = "1.0"
 """str: The version number of this module.
 Used to track compatibility and changes across releases.
 """
+
+def expected_meta_version(this_version:str) -> bool:
+    """Returns a bool if the input version matches the meta version of the library framework it was run on"""
+    return module_version == this_version
