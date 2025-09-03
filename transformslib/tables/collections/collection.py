96292d25507cdb5a895c90c306d012d420026c02
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-09-04 01:04:39 +1000

    fix on writing

--- transformslib/tables/collections/collection.py
+++ transformslib/tables/collections/collection.py
@@ -406,7 +406,8 @@ class TableCollection:
 
         for table in self.tables:
             output_path = os.path.join(output_dir, table.table_name + ".parquet")
-            table.write(path=Path(output_path), spark=spark)
+            output_path = str(Path(output_path))
+            table.write(path=output_path, spark=spark)
         
         self.save_events()
                 
