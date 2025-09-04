36fda523067208b6c3ce569bfe93b59322053025
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-09-04 15:07:49 +1000

    new make dag script

---
+++ templates/make_dag.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    import os
+    import sys
+    # Get the directory of the current script
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    # Add the parent directory to sys.path
+    parent_dir = os.path.join(current_dir, '..')
+    sys.path.append(os.path.abspath(parent_dir))
+
+    from transformslib.mapping import dag
+    dag.build_dag(1, 1)
