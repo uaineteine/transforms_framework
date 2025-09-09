b2a0e458dc411e37b9e177e30d662a2e44dfd4f9
Daniel Stamer-Squair <daniel.robert.s-s@hotmail.com>
2025-09-10 09:04:00 +1000

    test fixes

--- templates/template_atomic_pipe.py
+++ templates/template_atomic_pipe.py
@@ -171,11 +171,11 @@ if __name__ == "__main__":
     # -------------------------------
     print("Truncating a date to year and month levels")
     # Convert string to date
-    supply_frames["date_table"].df = supply_frames["date_table"].df.withColumn("event_date", to_date("event_date", "yyyy-MM-dd"))
+    supply_frames["date_table"].df = supply_frames["date_table"].df.withColumn("EVENT_DATE", to_date("EVENT_DATE", "yyyy-MM-dd"))
     
-    supply_frames = TruncateDate("date", "year").apply(supply_frames, df="date_table")
+    supply_frames = TruncateDate("EVENT_DATE", "year").apply(supply_frames, df="date_table")
     supply_frames["date_table"].show()
-    supply_frames = TruncateDate("date", "month").apply(supply_frames, df="date_table")
+    supply_frames = TruncateDate("EVENT_DATE", "month").apply(supply_frames, df="date_table")
     supply_frames["date_table"].show()
     #print(supply_frames["date_table"].dtypes)
 
