2e3929fcf9c6b0b287405608a2b20a0811fee71d
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-09-14 16:26:41 +1000

    modify output location

--- templates/template_atomic_pipe.py
+++ templates/template_atomic_pipe.py
@@ -208,4 +208,4 @@ if __name__ == "__main__":
     supply_frames["super_table"].show()
 
     # save table output tables
-    supply_frames.save_all("../test_tables/output", spark=spark)
+    supply_frames.save_all(f"../test_tables/prod/job_{job_id}output", spark=spark)
