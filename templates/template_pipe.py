d50517d9005990647392a1f22b291f13004d8803
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-10-13 18:52:39 +1100

    fix

--- templates/template_pipe.py
+++ templates/template_pipe.py
@@ -212,7 +212,7 @@ if __name__ == "__main__":
     # Test 18: TopBottomCoding
     #   -------------------------------
     print("Applying TopBottomCode macro to salary column")
-    supply_frames = TopBottomCode(supply_frames, ["income"], 500, 450).apply(supply_frames)
+    supply_frames = TopBottomCode(supply_frames, ["income"], 500, 450).apply()
     print("Original salary data:")
     supply_frames["salary"].show()
 
