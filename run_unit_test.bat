e94c90bba66a2b203ef535973bbe6e00f07b9af2
Daniel Stamer-Squair <daniel.robert.s-s@hotmail.com>
2025-11-21 15:32:21 +1100

    unit tests now append to logs

--- run_unit_test.bat
+++ run_unit_test.bat
@@ -11,10 +11,13 @@ for /d /r %%d in (__pycache__) do (
 
 echo Done.
 
+echo Removing the previous test_log.txt file if it exists...
+if exist tests\test_log.txt del tests\test_log.txt
+
 cd tests
 cd scripts
 
-powershell -Command "python list_transforms.py | Tee-Object -FilePath ../../tests/test_log.txt"
+powershell -Command "python list_transforms.py | Tee-Object -FilePath ../../tests/test_log.txt -Append"
 
 cd ..
 cd ..
@@ -23,8 +26,8 @@ cd templates
 
 rmdir /S /Q jobs
 
-powershell -Command "python template_pipe.py | Tee-Object -FilePath ../tests/test_log.txt"
+powershell -Command "python template_pipe.py | Tee-Object -FilePath ../tests/test_log.txt -Append"
 
-powershell -Command "python make_dag.py | Tee-Object -FilePath ../tests/test_log.txt"
+powershell -Command "python make_dag.py | Tee-Object -FilePath ../tests/test_log.txt -Append"
 
 cd ..
