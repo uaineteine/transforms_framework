4900cdf356c9caf67cbd28adb43712185cc3d6f3
Daniel Stamer-Squair <daniel.robert.s-s@hotmail.com>
2025-11-27 09:49:09 +1100

    expand unit tests

--- tests/scripts/save_raw_text.py
+++ tests/scripts/save_raw_text.py
@@ -1,10 +1,14 @@
 test_str = "this is a test file"
 path = "test_tables/unit_test_subpackage/this_is_a_test.txt"
 
+import os
 from pyspark.sql import SparkSession
-from adaptiveio import save_raw_text, read_raw_text
+from adaptiveio import save_raw_text, read_raw_text, normalisePaths
 
 sparkSession = SparkSession.builder.master("local").appName("adio").getOrCreate()
+
+path = normalisePaths(path)
+os.makedirs(os.path.dirname(path))
 
 save_raw_text(path, test_str, spark=sparkSession)
 
