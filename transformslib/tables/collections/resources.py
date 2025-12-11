827493ac5d8aaf5a73944c4a7fb06164af6a8e6f
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2025-12-11 15:06:12 +1100

    load_ent_map methods added

--- transformslib/tables/collections/resources.py
+++ transformslib/tables/collections/resources.py
@@ -1,4 +1,13 @@
+from transformslib.engine import get_engine, get_spark
 from transformslib.templates.pathing import apply_formats
+from transformslib.tables.metaframe import MetaFrame
+
+import pandas as pd
+import polars as pl
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import reduce
+
+import os
 
 def load_specific_ent_map(id_group:int) -> MetaFrame:
     """
@@ -14,7 +23,8 @@ def load_specific_ent_map(id_group:int) -> MetaFrame:
     map_path = f"{map_path}/dss_entity_map_view/id_group={id_group}"
 
     tn = "entity_map_{id_group}"
-    df = MetaFrame.load(map_path, format="parquet", table_name=tn, spark=get_spark())
+    engine = get_engine()
+    df = MetaFrame.load(map_path, format="parquet", table_name=tn, frame_type=engine, spark=get_spark())
     return df
 
 def load_ent_map(id_groups:list[int]) -> MetaFrame:
@@ -25,8 +35,34 @@ def load_ent_map(id_groups:list[int]) -> MetaFrame:
 
     Returns: Metaframe of the id groups
     """
-    frames = []
-    for id in id_groups:
-        frames.append(load_specific_ent_map(id))
+    #deduplicate the id groups first
+    #this will reduce the amount of frames to concat
+    id_groups = list(set(id_groups))
+
+    #if using multiple id groups
+    if len(id_groups) > 1:
+        frames = []
+        for id in id_groups:
+            frames.append(load_specific_ent_map(id))
+        
+        #append these frames together
+        engine = get_engine()
+        if engine == "pandas":
+            df = pd.concat(frames, ignore_index=True)
+
+        elif engine == "polars":
+            df = pl.concat(frames)
+
+        elif engine == "pyspark":
+            df = reduce(DataFrame.unionAll, frames)
+
+        else:
+            raise NotImplementedError(f"RS001 Entity mapping appendage not implemented for backend '{engine}'")
+    #there is only 1 df
+    else:
+        df = load_specific_ent_map(id)
+
+    #deuplicate for return
+    return df.distinct()
+
     
-    #append these frames together
