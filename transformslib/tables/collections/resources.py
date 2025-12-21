e02ae659db1cef64262b29f45fef3991416a3198
Daniel Stamer-Squair <daniel.robert.s-s@hotmail.com>
2025-12-21 23:36:12 +1100

    using mt adoption of concatlist

--- transformslib/tables/collections/resources.py
+++ transformslib/tables/collections/resources.py
@@ -1,7 +1,7 @@
 from transformslib.engine import get_engine, get_spark
 from transformslib.templates.pathing import apply_formats
 from transformslib.tables.metaframe import MetaFrame
-from multitable import MultiTable
+from multitable import MultiTable, concatlist
 
 import pandas as pd
 import polars as pl
@@ -9,31 +9,6 @@ import polars as pl
 from functools import reduce
 
 import os
-
-def concat(frames:list[MultiTable], engine:str) -> MultiTable:
-    if not frames:
-        raise ValueError("No frames to concatenate")
-    native_frames = [f.df for f in frames]
-
-    if engine == "pandas":
-        combined = pd.concat(native_frames, ignore_index=True)
-
-    elif engine == "polars":
-        combined = pl.concat(native_frames)
-
-    elif engine == "pyspark":
-        # Safe union across all frames
-        if len(native_frames) == 1:
-            combined = native_frames[0]
-        else:
-            combined = reduce(lambda df1, df2: df1.union(df2), native_frames)
-
-    else:
-        raise NotImplementedError(
-            f"RS400 Metaframe appendage not implemented for backend '{engine}'"
-        )
-    
-    return MultiTable(combined, src_path=frames[0].src_path, table_name=frames[0].table_name, frame_type=engine)
 
 def load_specific_ent_map(id_group:int) -> MultiTable:
     """
@@ -81,7 +56,7 @@ def load_ent_map(id_groups:list[int]) -> MetaFrame:
             frames.append(load_specific_ent_map(id))
         
         #append these frames together
-        df = concat(frames, engine)
+        df = concatlist(frames, engine)
     
     #there is only 1 df
     else:
