43d31f35007ff32851c375f7c83ecc75faf0bb8b
Daniel Stamer-Squair <daniel.robert.s-s@hotmail.com>
2026-01-23 23:42:00 +1100

    update batch file to remove binary format and output text in utf-8

--- run_unit_test.bat
+++ run_unit_test.bat
@@ -15,6 +15,8 @@ echo Done.
 echo Removing the previous test_log.txt file if it exists...
 if exist tests\test_log.txt del tests\test_log.txt
 
+echo Starting unit tests...
+
 :: Check if --express flag is passed
 if "%1"=="--express" goto EXPRESS_MODE
 
@@ -28,7 +30,7 @@ echo Running: save_raw_text.py >> ../../tests/test_log.txt
 echo ================================================================================ >> ../../tests/test_log.txt
 echo. >> ../../tests/test_log.txt
 
-powershell -Command "$output = python save_raw_text.py 2>&1; $exitCode = $LASTEXITCODE; $output | Tee-Object -FilePath ../../tests/test_log.txt -Append; exit $exitCode"
+powershell -Command "$output = python save_raw_text.py 2>&1; $exitCode = $LASTEXITCODE; $output = $output -replace '[^\u0000-\u007F]', ''; $output | Out-String -Stream | Out-File -FilePath ../../tests/test_log.txt -Encoding utf8 -Append; exit $exitCode"
 if %ERRORLEVEL% NEQ 0 (
     echo save_raw_text.py failed with exit code %ERRORLEVEL%
     cd ..\..
@@ -42,7 +44,7 @@ echo Running: list_transforms.py >> ../../tests/test_log.txt
 echo ================================================================================ >> ../../tests/test_log.txt
 echo. >> ../../tests/test_log.txt
 
-powershell -Command "$output = python list_transforms.py 2>&1; $exitCode = $LASTEXITCODE; $output | Tee-Object -FilePath ../../tests/test_log.txt -Append; exit $exitCode"
+powershell -Command "$output = python list_transforms.py 2>&1; $exitCode = $LASTEXITCODE; $output = $output -replace '[^\u0000-\u007F]', ''; $output | Out-String -Stream | Out-File -FilePath ../../tests/test_log.txt -Encoding utf8 -Append; exit $exitCode"
 if %ERRORLEVEL% NEQ 0 (
     echo list_transforms.py failed with exit code %ERRORLEVEL%
     cd ..\..
@@ -56,7 +58,7 @@ echo Running: load_resources.py >> ../../tests/test_log.txt
 echo ================================================================================ >> ../../tests/test_log.txt
 echo. >> ../../tests/test_log.txt
 
-powershell -Command "$output = python load_resources.py 2>&1; $exitCode = $LASTEXITCODE; $output | Tee-Object -FilePath ../../tests/test_log.txt -Append; exit $exitCode"
+powershell -Command "$output = python load_resources.py 2>&1; $exitCode = $LASTEXITCODE; $output = $output -replace '[^\u0000-\u007F]', ''; $output | Out-String -Stream | Out-File -FilePath ../../tests/test_log.txt -Encoding utf8 -Append; exit $exitCode"
 if %ERRORLEVEL% NEQ 0 (
     echo load_resources.py failed with exit code %ERRORLEVEL%
     cd ..\..
@@ -79,7 +81,7 @@ echo Running: template_pipe.py >> ../tests/test_log.txt
 echo ================================================================================ >> ../tests/test_log.txt
 echo. >> ../tests/test_log.txt
 
-powershell -Command "$output = python template_pipe.py 2>&1; $exitCode = $LASTEXITCODE; $output | Tee-Object -FilePath ../tests/test_log.txt -Append; exit $exitCode"
+powershell -Command "$output = python template_pipe.py 2>&1; $exitCode = $LASTEXITCODE; $output = $output -replace '[^\u0000-\u007F]', ''; $output | Out-String -Stream | Out-File -FilePath ../tests/test_log.txt -Encoding utf8 -Append; exit $exitCode"
 if %ERRORLEVEL% NEQ 0 (
     echo template_pipe.py failed with exit code %ERRORLEVEL%
     cd ..
@@ -93,7 +95,7 @@ echo Running: make_dag.py >> ../tests/test_log.txt
 echo ================================================================================ >> ../tests/test_log.txt
 echo. >> ../tests/test_log.txt
 
-powershell -Command "$output = python make_dag.py 2>&1; $exitCode = $LASTEXITCODE; $output | Tee-Object -FilePath ../tests/test_log.txt -Append; exit $exitCode"
+powershell -Command "$output = python make_dag.py 2>&1; $exitCode = $LASTEXITCODE; $output = $output -replace '[^\u0000-\u007F]', ''; $output | Out-String -Stream | Out-File -FilePath ../tests/test_log.txt -Encoding utf8 -Append; exit $exitCode"
 if %ERRORLEVEL% NEQ 0 (
     echo make_dag.py failed with exit code %ERRORLEVEL%
     cd ..
@@ -113,7 +115,7 @@ cd tests
 cd scripts
 
 :: Call the batch file and log its output live + to file
-powershell -Command "$output = & { python .\chck_sprk_mtdat.py } 2>&1; $exitCode = $LASTEXITCODE; $output | Tee-Object -FilePath ../../tests/test_log.txt -Append; exit $exitCode"
+powershell -Command "$output = & { python .\chck_sprk_mtdat.py } 2>&1; $exitCode = $LASTEXITCODE; $output = $output -replace '[^\u0000-\u007F]', ''; $output | Out-String -Stream | Out-File -FilePath ../../tests/test_log.txt -Encoding utf8 -Append; exit $exitCode"
 
 if %ERRORLEVEL% NEQ 0 (
     echo chck_sprk_mtdat.py failed with exit code %ERRORLEVEL%
